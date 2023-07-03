c328540246fcc76d637237294543a8d337794b14
JLaydeJ <134284646+JLaydeJ@users.noreply.github.com>
2023-07-03 15:39:23 -0400

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,9 +9,6 @@ Months = []
 Profit_Losses = []
 Profit_Losses_Change = []
 
-#Set inital values
-#Total_Months = 0 
-
 #Open CSV file
 with open(budget_csv_path, newline='') as csvfile:
     #Seperate data by comma ','
@@ -20,50 +17,53 @@ with open(budget_csv_path, newline='') as csvfile:
     #Skip the header row
     next(budget_reader)
 
-    #Create table that excludes first row
-    Budget_Table = [next(budget_reader)]
+    #Create table minus first row
+    First_Row = next(budget_reader)
+ 
+    #Add first row profit/losses value to list
+    Profit_Losses.append(int(First_Row[1]))
 
-    #Set inital previous profit/losses net amounts
-    Previous_Total = int(Budget_Table[0][1])
+    #Set inital previous profit/losses net amounts with first row value
+    Previous_Total = int(First_Row[1])
 
-    #Set initial number of months excluded in table created 
-    Initial_Month = 1
 
-    #Set initial number of profit/losses excluded in table created 
-    Initial_Profit_Losses = 1088983
-    
     #Create Loop to loop through CSV File
     for row in budget_reader:
+
         #Add months/dates
         Months.append(row[0])
-
-        #The total number of months included in the dataset plus inital month
-        Total_Months = len(Months) + Initial_Month
        
         #Add profit/losses
         Profit_Losses.append(int(row[1]))
-
-        #The net total amount of "Profit/Losses" over the entire period
-        Total_ProfLoss = sum(Profit_Losses) + Initial_Profit_Losses
 
         #Tried setting Changes = 0 and got syntac error
         #Tried setting Changes = [] inside loop but got -224669.0, so I put Changes = [] outside loop
         #Add changes ; The changes in "Profit/Losses" over the entire period
         Profit_Losses_Change.append(int(row[1]) - Previous_Total)
 
-        #The average of those changes
-        Average = sum(Profit_Losses_Change) / len(Profit_Losses_Change)
-
         #Re-define/Store new previous profit/losses net amount
         Previous_Total = int(row[1]) 
+
+    #Calculations
+    #Add first row months/dates to list 
+    Months.append(First_Row[0])
+
+    #The total number of months included in the dataset
+    Total_Months = len(Months)
+
+    #The net total amount of "Profit/Losses" over the entire period
+    Total_Profit_Losses = sum(Profit_Losses) 
+
+    #The average of those changes
+    Average = sum(Profit_Losses_Change) / len(Profit_Losses_Change)
 
     #The greatest increase in profits amount over the entire period (use max function)
     Greatest_Increase_Profit = max(Profit_Losses_Change)
 
-    #The greatest decrease in profits amount over the entire period (use max function)
+    #The greatest decrease in profits amount over the entire period (use min function)
     Greatest_Decrease_Profit = min(Profit_Losses_Change)
     
-    #The greatest increase/decrease in profit dates (use index function)
+    #The greatest increase/decrease in profit dates and amount (use index function)
     Increase_Index = Profit_Losses_Change.index(Greatest_Increase_Profit)
     Decrease_Index = Profit_Losses_Change.index(Greatest_Decrease_Profit)
     Greatest_Increase_Dates = Months[Increase_Index]
@@ -71,9 +71,11 @@ with open(budget_csv_path, newline='') as csvfile:
 
 #Print output to Terminal
 Print = f'''Financial Analysis\n-------------------------
-Total Months: {Total_Months}\nTotal: {Total_ProfLoss}\nAverage Change: ${Average:.2f}
-Greatest Increase in Profits: {Greatest_Increase_Dates} (${Greatest_Increase_Profit}
+Total Months: {Total_Months}\nTotal: {Total_Profit_Losses}\nAverage Change: ${Average:.2f}
+Greatest Increase in Profits: {Greatest_Increase_Dates} (${Greatest_Increase_Profit})
 Greatest Increase in Profits: {Greatest_Decrease_Dates} (${Greatest_Decrease_Profit})'''
+
+#Print to terminal
 print(Print)
 
 # Set variable for output file
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,6 +14,9 @@ Candidate_List = []
 #Dictionary to store data
 Votes_Per_Candidate = {}
 
+#Set initial value for winner
+Winning_Votes = 0 
+
 #Open CSV file
 with open(election_csv_path, newline='') as csvfile:
     #Seperate data by comma ','
@@ -24,70 +27,79 @@ with open(election_csv_path, newline='') as csvfile:
     
     #Create Loop to loop through CSV File 
     for row in election_reader:
+
         #Add Votes
         Votes.append(row[2])
         
         #The total number of votes cast
         Total_Votes = len(Votes)
  
-        #Set row 2 to candidates to refer to them by name
+        #Set row 2 to candidate to refer to them by name
         Candidates_Name = row[2]
 
     #A complete list of candidates who received votes
     #The total number of votes each candidate won
         if Candidates_Name not in Candidate_List:
 
-            #Add Candidate List
+            #Add names to Candidate List
             Candidate_List.append(Candidates_Name)
-        
-            #Add candidate names and votes per candidate 
-            Votes_Per_Candidate[Candidates_Name] = 1
-        else:
-            Votes_Per_Candidate[Candidates_Name] += 1
-    #Print progress to Terminal
-    print1= f'''Election Results
--------------------------
-Total Votes: {Total_Votes}
--------------------------\n'''
-    print(print1)
 
+            #Start keeping track of candidate's vote
+            Votes_Per_Candidate[Candidates_Name] = 1
+
+        else:
+            #Add vote to candidate's total vote count
+            Votes_Per_Candidate[Candidates_Name] += 1
+
+# Open the output file
+with open(budget_csv_path, "w") as textfile:
+    #Print total votes to Terminal
+    output=( f'Election Results\n'
+            f'-------------------------\n'
+            f'Total Votes: {Total_Votes}\n'
+            f'-------------------------\n')
+    
+    #Print output to terminal
+    print(output)
+
+    #Write output to textfile
+    textfile.write(output)
 
     #Create for statement to find percentage of votes per candidate
     for Candidates_Name in Votes_Per_Candidate:
 
-        #Pull Candidates Name and Total Votes per Candidate data to then calculate percentage  
+        #Pull Candidates Name and Total Votes Per Candidate data to then calculate Percentage  
         #Used get function to pull from dictionary created
         Candidate_Total_Votes = Votes_Per_Candidate.get(Candidates_Name)
 
         #The percentage of votes each candidate won
         Percentage = Candidate_Total_Votes / Total_Votes * 100
 
-        #Print progress to terminal
-        print2 = (f"{Candidates_Name} : {Percentage:.2f}% ({Candidate_Total_Votes})")
-        print(print2)
+        #Print candidate's names, percentages and total votes to terminal
+        output2 = (f"{Candidates_Name} : {Percentage:.3f}% ({Candidate_Total_Votes})\n")
 
+        #Print output2 to terminal
+        print(output2)
 
-    #The winner of the election based on popular vote
-    Winner = max(Candidate_List)
+        #Write output2 to textfile
+        textfile.write(output2)
+
+        #The winner of the election based on popular vote
+        #Create if state to determine the winner 
+        if Candidate_Total_Votes > Winning_Votes: 
+
+            #Winning candidate has vote equal to popular vote
+            Winning_Votes = Candidate_Total_Votes
+
+            #State Winner by Name
+            Winner = Candidates_Name
 
     #Print progress to terminal
-    print3= '''\n-------------------------
-'''
-    print4 = f'''Winner: {Winner}\n-------------------------'''
+    output3= (f'-------------------------\n'
+              f'Winner: {Winner}\n-------------------------')
     
-    print(print3)
-    print(print4)
+    #Print output3 to terminal
+    print(output3)
 
-
-#  Open the output file
-with open(budget_csv_path, "w") as textfile:
-
-    #Write to textfile
-    textfile.write(print1)
-    textfile.write(print2)
-    textfile.write(print3)
-    textfile.write(print4)
-
-
-
-    
+    #Write output3 to textfile
+    textfile.write(output3)
